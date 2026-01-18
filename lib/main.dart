d92e85031ce77fbe24699cc521964884f071f430
嗚呼起飛 <zxr52090114@gmail.com>
2026-01-18 16:44:19 +0800

    這樣是對的

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: HomePage()));
+  runApp(const MyApp());
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Phoho Show', style: TextStyle(color: Color(0xFFFFF600))),
-      ),
-      body: Column(
-        children: [
-          Image.network(
-            'https://cdn.pixabay.com/photo/2016/12/11/12/02/bled-1899264_960_720.jpg',
-          ),
-          Image.network(
-            'https://cdn.pixabay.com/photo/2017/01/19/23/46/panorama-1993645_960_720.jpg',
-          ),
-          Image.network(
-            'https://cdn.pixabay.com/photo/2017/12/15/13/51/polynesia-3021072_960_720.jpg',
-          ),
-        ],
-      ),
+    return MaterialApp(
+      title: 'Chikawa Airport',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(title: 'Chikawa Airport Home Page'),
     );
   }
 }
